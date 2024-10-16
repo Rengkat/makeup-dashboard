a24785252e5a7a286c50c9cf6ee525a2557e5534
Rengkat <successinmotion11@gmail.com>
2024-10-16 22:45:21 +0100

    pages setup

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,7 +1,32 @@
+import Link from "next/link";
 import React from "react";
 
 const SideBar = () => {
-  return <div>SideBar</div>;
+  return (
+    <div className="mt-[6rem]">
+      <Link className="block" href={"#"}>
+        Dashboard
+      </Link>
+      <Link className="block" href={"#"}>
+        Orders & Sales Management
+      </Link>
+      <Link className="block" href={"#"}>
+        Product Management
+      </Link>
+      <Link className="block" href={"#"}>
+        Appointment Management
+      </Link>
+      <Link className="block" href={"#"}>
+        Client Management
+      </Link>
+      <Link className="block" href={"#"}>
+        Settings
+      </Link>
+      <Link className="block" href={"#"}>
+        Payment Management
+      </Link>
+    </div>
+  );
 };
 
 export default SideBar;
