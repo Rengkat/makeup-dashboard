12d359b6a8041efe51dee2fe55780a554f7d6c20
Rengkat <successinmotion11@gmail.com>
2024-10-16 22:30:36 +0100

    deasktop nav

--- src/app/(dashboard)/layout.js
+++ src/app/(dashboard)/layout.js
@@ -4,14 +4,14 @@ import SideBar from "@/components/SideBar";
 export default function DashboardLayout({ children }) {
   return (
     <div className="relative bg-white">
-      <nav className=" bg-lighter-gold w-full h-[4rem] fixed z-20 top-0 left-0 right-0 shadow-md">
+      <nav className=" bg-lighter-gold w-full fixed z-20 top-0 left-0 right-0 shadow-md">
         <Nav />
       </nav>
       <div>
         <aside className="bg-lighter-gold h-screen w-[20rem] fixed left-0 bottom-0 shadow-md">
           <SideBar />
         </aside>
-        <main className="ml-[20rem] mt-[4rem]">{children}</main>
+        <main className="ml-[20rem] mt-[6rem]">{children}</main>
       </div>
     </div>
   );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,25 @@
+import Image from "next/image";
 import React from "react";
-
+import { IoNotifications } from "react-icons/io5";
+import { FaUserAlt } from "react-icons/fa";
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <div className="h-[6rem] flex justify-between items-center py-2 px-[5rem]">
+      <Image src={"/Fullbeauty.svg"} alt="logo" height={500} width={500} className="w-[6%]" />
+      <div className="flex items-center gap-8">
+        <div className="relative cursor-pointer">
+          <IoNotifications fontSize={35} className="text-dark-gold" />
+          <div className="bg-white absolute top-1 right-1 rounded-full p-[1px]">
+            <div className="size-2 bg-red-700 rounded-full shadow" />
+          </div>
+        </div>
+        <div className="flex items-end gap-2">
+          <FaUserAlt fontSize={30} className="text-dark-gold" />
+          <span className="text-dark-green font-semibold">Alexander</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Nav;
